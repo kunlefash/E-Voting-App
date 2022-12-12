9116eed6f8f54fabaf2a3e90b4b51f3fb0a4828a
kunlefash <kunlefashmayop@gmail.com>
2022-12-12 04:03:59 -0400

    Create and style footer.js component

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          View this project on{" "}
+          <a
+            className="profile"
+            href="https://github.com/kunlefash/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <p>
+          Made with <i className="fas fa-heartbeat" /> by{" "}
+          <a
+            className="profile"
+            href="https://github.com/kunlefash/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Adekunle Fasakin
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
