1d6d1ab947156ddda98bbf54fc101af0a77ba787
kunlefash <kunlefashmayop@gmail.com>
2023-01-12 03:50:30 -0400

    working on truffle migrate

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Migration = artifacts.require("./Migration.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migration);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -58,22 +58,17 @@ module.exports = {
    */
 
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache, geth, or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
     //
     // An additional network, but with some advanced options…
     // advanced: {
