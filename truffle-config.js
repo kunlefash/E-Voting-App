d76c2a5e7a4af485e1b2751b1967cce8419b5472
kunlefash <kunlefashmayop@gmail.com>
2023-01-12 10:25:07 -0400

    install hdwallet-provider

--- truffle-config.js
+++ truffle-config.js
@@ -97,6 +97,7 @@ module.exports = {
      }),
        network_id: 5,       // Goerli's id
        confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+       networkCheckTimeout: 10000,
        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
    },
