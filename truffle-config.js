4bd90c1e0968d30547dce7e6a2a4e3088fd8329b
kunlefash <kunlefashmayop@gmail.com>
2023-01-12 04:51:51 -0400

    install dotenv and input secret keys

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -46,6 +48,11 @@
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+const private_keys = [
+  process.env.PRIVATE_KEY_0,
+  process.env.PRIVATE_KEY_1,
+]
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -82,13 +89,17 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+       provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: 'https://goerli.infura.io/v3/4ddf381e29a244e18e233a9798b0a187',
+        numberOfAddress: 2
+     }),
+       network_id: 5,       // Goerli's id
+       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+   },
     //
     // Useful for private networks
     // private: {
